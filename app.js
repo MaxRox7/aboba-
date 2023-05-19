cd7fee450d15c6e3bb08a27280d8f0a3aa0d0717
Maxim Boldyrev <maxrox1904@gmail.com>
2023-05-20 00:02:00 +0300

    1.01

--- app.js
+++ app.js
@@ -4,8 +4,24 @@ const port = 3000
 const bodyParser = require('body-parser')
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const engines = require('consolidate');
+const fs = require ('fs')
 const token_from_server = 45678
 
+
+
+const SaveData = (token) => {
+  const finished = (error) => {
+    if (error) {
+      console.errror (error)
+      return;
+    }
+  }
+    let jsonData = JSON.stringify(token)
+    fs.writeFileSync('/application','token.txt','utf8', jsonData, finished)
+  console.log('File saved')
+  
+
+}
 
 app.engine('html', engines.mustache);
 app.set('view engine', 'ejs');
@@ -32,6 +48,10 @@ app.post('/itsme', urlencodedParser, (req,res) => {
 if (!req.body) return res.sendStatus(400);
 const obj = JSON.parse(JSON.stringify(req.body)); 
 console.log(obj); 
+SaveData(obj);
+
 res.render('itsme-passed', {data: req.body})
+
+
 
 })
